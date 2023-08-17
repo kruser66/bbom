d0cdb2624a65d77d48c49122caaf66ecbd0ff627
kruser <kruser@yandex.ru>
2023-08-17 12:29:33 +0500

    fixed flake8

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from blog.managers import CustomUserManager
 
+
 class User(AbstractUser):
     username = None
     id = models.UUIDField(
@@ -20,19 +21,19 @@ class User(AbstractUser):
     )
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
-    
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-    
+
     objects = CustomUserManager()
-    
+
     class Meta:
         verbose_name = 'пользователь'
         verbose_name_plural = 'пользователи'
 
     def __str__(self):
         return self.email
-    
+
 
 class Post(models.Model):
     id = models.UUIDField(
@@ -52,8 +53,7 @@ class Post(models.Model):
     body = models.TextField(
         'Тело поста'
     )
-    
-    
+
     class Meta:
         verbose_name = 'пост'
         verbose_name_plural = 'посты'
--- config/urls.py
+++ config/urls.py
@@ -3,15 +3,15 @@ from django.contrib import admin
 from rest_framework import routers
 from blog import views
 
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet, basename='user')
 router.register(r'posts', views.PostViewSet, basename='post')
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', include('blog.urls'))    
+    path('', include('blog.urls')),
 ]
